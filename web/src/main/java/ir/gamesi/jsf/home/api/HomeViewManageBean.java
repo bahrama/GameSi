6269e28ed0d8336db15b63a39025868013ebae44
Ali The Boss <alialikhahasl@gmail.com>
2023-10-30 23:48:28 +0330

    shab5

--- web/src/main/java/ir/gamesi/jsf/home/api/HomeViewManageBean.java
+++ web/src/main/java/ir/gamesi/jsf/home/api/HomeViewManageBean.java
@@ -6,14 +6,15 @@ import jakarta.annotation.Resource;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
-import jakarta.mail.Message;
-import jakarta.mail.Session;
-import jakarta.mail.Transport;
+import jakarta.mail.*;
 import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeBodyPart;
 import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Properties;
 
 @ViewScoped
 @Named
@@ -28,18 +29,40 @@ public class HomeViewManageBean implements Serializable {
     }
 
     public void actBoss() {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "mail.gamesi.ir");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.ssl.trust", "mail.gamesi.ir");
+        Session session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("admin@gamesi.ir", "ali680313ALI^*)#!#");
+            }
+        });
+
+        Message message = new MimeMessage(session);
         try {
-            System.out.println("----------------send------------------");
-            Message message = new MimeMessage(sessionSend);
-            System.out.println("----------------2------------------");
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("alialikhahasl@gmail.com"));
-            System.out.println("----------------3------------------");
-            //  message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("ali_alikhahasl@yahoo.com"));
-            message.setContent("salalallalalallalallam","text/html; charset=UTF-8");
-            System.out.println("----------------4------------------");
+            message.setFrom(new InternetAddress("admin@gamesi.ir"));
+            message.setRecipients(
+                    Message.RecipientType.TO, InternetAddress.parse("alialikhahasl@gmail.com"));
+            message.setSubject("Mail Subject");
+
+            String msg = "This is my first email using JavaMailer";
+
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setContent(msg, "text/html; charset=utf-8");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
+
+            message.setContent(multipart);
+
             Transport.send(message);
-            System.out.println("----------------sended------------------");
-        } catch (Exception e) {
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
         }
+
     }
 }
