2c513598bf614d478bbc86e52fac1061fb34c6e9
Ali The Boss <alialikhahasl@gmail.com>
2023-11-06 22:32:32 +0330

    shab8

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-echo "Starting paano front project ...."
-
+echo "Starting wildfly project ...."
 git clone https://github.com/bahrama/GameSi.git
-
 git pull origin main
-
 mvn clean
-
 mvn install
-
+bash
