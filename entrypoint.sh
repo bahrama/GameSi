a9d59644704a2a263a40b0b32f485e8f77566735
Ali The Boss <alialikhahasl@gmail.com>
2023-11-06 23:15:23 +0330

    shab9

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 echo "Starting wildfly project ...."
 git clone https://github.com/bahrama/GameSi.git
 git pull origin main
-mvn clean
-mvn install
+cp -R GameSi/postgresql wildfly-30.0.0.Final/modules/system/layers/base/org/
+cp -R GameSi/persistance wildfly-30.0.0.Final/modules/system/layers/base/org/eclipse/
+cp GameSi/statndalone.xml wildfly-30.0.0.Final/standalone/configuration
+cd GameSi
+.././apache-maven-3.9.5/bin/mvn clean
+.././apache-maven-3.9.5/bin/mvn install
 bash
